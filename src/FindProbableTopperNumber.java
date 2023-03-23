32b2570cb5e4cce815c48876f6d7ae349755b910
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-03-23 21:29:50 +0530

    Add. Added the logic to find topper probable register number

--- src/FindProbableTopperNumber.java
+++ src/FindProbableTopperNumber.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class FindProbableTopperNumber {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the Register Number: ");
+        int registerNumber = scan.nextInt();
+
+        String iteratorStringRegisterNumber = Integer.toString(registerNumber);
+
+        int evenSum = 0,oddSum = 0;
+
+        for(int iterator = 0;iterator<iteratorStringRegisterNumber.length();iterator++){
+
+            if(iterator%2 == 0){
+
+                evenSum+=(iteratorStringRegisterNumber.charAt(iterator)-'0');
+
+            }else{
+
+                oddSum+=(iteratorStringRegisterNumber.charAt(iterator)-'0');
+
+            }
+
+        }
+
+        if(oddSum == evenSum){
+
+            System.out.println("Yes");
+
+        }else{
+
+            System.out.println("No");
+
+        }
+
     }
 }
